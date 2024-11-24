b18f10ed4d063bfe07d56a76e638c4e9aa31c879
Niksh-Hiremath <57857850+09ghostrider@users.noreply.github.com>
2024-11-24 06:57:36 +0530

    solutions.json and implenented /check-code

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Header, HTTPException, UploadFile, File, Cookie
+from fastapi import APIRouter, Header, HTTPException, UploadFile, File, Body
 from fastapi.responses import JSONResponse
 from typing import Annotated
 import os
@@ -7,6 +7,9 @@ from pathlib import Path
 from uuid import uuid4
 import whisper
 import openai
+from RestrictedPython import compile_restricted
+from RestrictedPython import safe_globals
+import json
 
 load_dotenv()
 
@@ -20,6 +23,52 @@ router = APIRouter()
 model = whisper.load_model("base")
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+SESSIONS = {}
+
+with open("solutions.json", "r") as f:
+    SOLUTIONS = json.load(f)
+
+
+def check_leetcode_solution(
+    code: str, func: str, solutions: list, *input: list
+) -> bool:
+    try:
+        compiled_code = compile_restricted(code, "<string>", "exec")
+        exec(compiled_code, safe_globals, {})
+
+        if "Solution" in safe_globals and hasattr(safe_globals["Solution"], func):
+            result = safe_globals["Solution"]().__getattribute__(func)(*input)
+            if result in solutions:
+                return True
+        return False
+    except Exception:
+        return True
+
+
+@router.post("/check-code")
+async def check_code(
+    authorization: str = Annotated[
+        str,
+        Header(..., description="Authorization Key", alias="Authorization"),
+    ],
+    question_id: str = Annotated[
+        str,
+        Body(..., description="LeetCode Question ID", alias="question_id"),
+    ],
+    code: str = Annotated[str, Body(..., description="Code to check", alias="code")],
+):
+    if authorization != AUTHORIZATION_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    func = SOLUTIONS[question_id]["func"]
+    for testcase in SOLUTIONS[question_id]["testcases"]:
+        if not check_leetcode_solution(
+            code, func, testcase["solutions"], *testcase["input"].values()
+        ):
+            return JSONResponse({"success": False, "result": "Incorrect"})
+    return JSONResponse({"success": True, "result": "Correct"})
 
 
 @router.post("/upload")
