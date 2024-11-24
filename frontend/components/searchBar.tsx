8d6f0192eaf7ccf45f772738c5174a96520a3fdd
Niksh-Hiremath <57857850+09ghostrider@users.noreply.github.com>
2024-11-24 06:15:44 +0530

    home page redesign

--- frontend/components/searchBar.tsx
+++ frontend/components/searchBar.tsx
@@ -11,15 +11,16 @@ function SearchBar({
 }) {
   return (
     <div className="flex justify-center w-full focus-within:border-[#0069FF] border border-solid rounded-full px-2 py-2 gap-2">
-      <Image src="/logo.png" width={36} height={36} alt="Search icon" />
+      {/* <Image src="/logo.png" width={36} height={36} alt="Search icon" /> */}
+
       <input
         type="text"
-        placeholder="Enter Your Problem ..."
+        placeholder="Enter LeetCode problem number..."
         value={search}
         onChange={(event) => {
           setSearch(event.target.value);
         }}
-        className="border-2 border-[#3E5C76] rounded-lg w-full bg-transparent text-white border-none focus:outline-none"
+        className="border-2 border-[#3E5C76] rounded-lg w-full bg-transparent text-white border-none focus:outline-none pl-3"
       />
     </div>
   );
